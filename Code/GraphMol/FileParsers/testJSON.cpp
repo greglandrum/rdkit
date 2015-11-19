d37c11006f6110413885983c30b1b5550a014734
Greg Landrum <greg.landrum@gmail.com>
2015-11-19 04:54:32 +0100

    more properties

--- Code/GraphMol/FileParsers/testJSON.cpp
+++ Code/GraphMol/FileParsers/testJSON.cpp
@@ -62,6 +62,49 @@ std::string basic_json_ethane =
     "      }"
     "   ]"
     "   }";
+std::string basic_json_ethanol_anion =
+    "{"
+    "   \"type\":\"mol\","
+    "   \"version\":\"0.9\","
+    "   \"title\":\"ethanol\","
+    "   \"dimension\":2,"
+    "   \"atomdefaults\":{"
+    "      \"implicithcount\":3,"
+    "      \"formalcharge\":0,"
+    "      \"stereo\":\"Undefined\""
+    "   },"
+    "   \"atoms\":["
+    "      {"
+    "         \"element\":6,"
+    "         \"coords\":["
+    "            0.0,"
+    "            0.0"
+    "         ]"
+    "      },"
+    "      {"
+    "         \"element\":8,"
+    "         \"coords\":["
+    "            0.0,"
+    "            1.0"
+    "         ],"
+    "         \"implicithcount\":0,"
+    "         \"formalcharge\":-1"
+    "      }"
+    "   ],"
+    "   \"bonddefaults\":{"
+    "      \"stereo\" : \"Undefined\","
+    "      \"order\":1"
+    "   },"
+    "   \"bonds\":["
+    "      {"
+    "         \"atoms\":["
+    "            0,"
+    "            1"
+    "         ],"
+    "         \"order\":1"
+    "      }"
+    "   ]"
+    "   }";
 // basic example from Brian Cole
 std::string basic_json_phenol =
     "{"
@@ -201,6 +244,26 @@ void testBasics() {
     delete mol;
   }
 
+  {
+    // std::cerr << local_data::basic_json_phenol.substr(1520, 50) << std::endl;
+    RWMol *mol = JSONToMol(local_data::basic_json_ethanol_anion);
+    TEST_ASSERT(mol);
+    std::cerr << MolToSmiles(*mol) << std::endl;
+    TEST_ASSERT(mol->hasProp("_Name"));
+    TEST_ASSERT(mol->getProp<std::string>("_Name") == "ethane");
+    TEST_ASSERT(mol->getNumAtoms() == 2);
+    TEST_ASSERT(mol->getNumBonds() == 1);
+    TEST_ASSERT(mol->getNumConformers() == 1);
+    TEST_ASSERT(feq(mol->getConformer().getAtomPos(0).x, 0.0));
+    TEST_ASSERT(feq(mol->getConformer().getAtomPos(0).y, 0.0));
+    TEST_ASSERT(feq(mol->getConformer().getAtomPos(1).x, 0.0));
+    TEST_ASSERT(feq(mol->getConformer().getAtomPos(1).y, 1.0));
+    TEST_ASSERT(mol->getAtomWithIdx(0)->getNumExplicitHs() == 3);
+    TEST_ASSERT(mol->getAtomWithIdx(1)->getNumExplicitHs() == 0);
+    TEST_ASSERT(mol->getAtomWithIdx(0)->getFormalCharge() == 0);
+    TEST_ASSERT(mol->getAtomWithIdx(1)->getFormalCharge() == -1);
+    delete mol;
+  }
   {
     // std::cerr << local_data::basic_json_phenol.substr(1520, 50) << std::endl;
     RWMol *mol = JSONToMol(local_data::basic_json_phenol);
