dc653af04a549fab7ba9b66dd089e2872fd2d47c
Greg Landrum <greg.landrum@gmail.com>
2016-06-05 07:40:26 +0200

    first pass through RDGeneral and DataStructs. No headers done yet

--- Code/RDGeneral/testDict.cpp
+++ Code/RDGeneral/testDict.cpp
@@ -172,7 +172,7 @@ void testRDAny() {
   }
   {
     std::clock_t clock1 = std::clock();
-    boost::any *v=0, *vv;
+    boost::any *v=nullptr, *vv;
     for(int i=0;i<loops;++i) {
       vv = new boost::any(v?boost::any_cast<int>(*v) + i: i);
       delete v;
@@ -197,7 +197,7 @@ void testRDAny() {
 
   {
     std::clock_t clock1 = std::clock();
-    RDAny *v=0, *vv;
+    RDAny *v=nullptr, *vv;
     for(int i=0;i<loops;++i) {
       vv = new RDAny(v ?rdany_cast<int>(*v) + i : i);
       delete v;
@@ -273,7 +273,7 @@ void testRDAny() {
 
   {
     // Check pointers -- RDAny doesn't delete these, must do them manually
-    std::vector<int> *p = new std::vector<int>();
+    auto p = new std::vector<int>();
     p->push_back(100);
     RDAny v(p);
     RDAny vv(v);
@@ -289,7 +289,7 @@ void testRDAny() {
     TEST_ASSERT((*rdany_cast<std::vector<int> *>((const RDAny&)vv))[0] == 100);
     delete p;
 
-    std::map<int,int> *m = new std::map<int,int>();
+    auto m = new std::map<int,int>();
     (*m)[0] = 1;
     RDAny mv(m);
     // leaks
